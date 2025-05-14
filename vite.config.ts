df077a117122b187abc31fb28a5e7d304ea4901b
Sara Borges <sarakborges@outlook.com>
2025-05-14 07:57:32 -0300

    vite port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  server: {
+    port: 80
+  },
+
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
   }

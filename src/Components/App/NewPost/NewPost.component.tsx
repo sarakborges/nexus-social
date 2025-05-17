d49d1215700193da5b388898cc7d5030a7388308
Sara Borges <sarakborges@outlook.com>
2025-05-17 15:47:57 -0300

    added actions to new post

--- src/Components/App/NewPost/NewPost.component.tsx
+++ src/Components/App/NewPost/NewPost.component.tsx
@@ -1,10 +1,17 @@
-import { NEW_POST_PLACEHOLDER } from '@/Consts/Feed.const'
+import {
+  FEED_PUBLISH,
+  NEW_POST_ADD_TO_POST,
+  NEW_POST_PLACEHOLDER
+} from '@/Consts/Feed.const'
 import { FIELD_TYPE_TEXTAREA } from '@/Consts/FieldTypes.const'
 
 import { CardComponent } from '@/Components/System/Card'
 import { FieldComponent } from '@/Components/System/Field'
+import { ButtonComponent } from '@/Components/System/Button'
 
 import './NewPost.style.scss'
+import { FaImage } from 'react-icons/fa'
+import { TypographyComponent } from '@/Components/System/Typography'
 
 export const NewPostComponent = () => (
   <div className="new-post">
@@ -13,6 +20,18 @@ export const NewPostComponent = () => (
         renderAs={FIELD_TYPE_TEXTAREA}
         placeholder={NEW_POST_PLACEHOLDER}
       />
+
+      <section className="new-post-actions">
+        <section className="new-post-actions-uploads">
+          <TypographyComponent>{NEW_POST_ADD_TO_POST}</TypographyComponent>
+
+          <ButtonComponent square transparent>
+            <FaImage />
+          </ButtonComponent>
+        </section>
+
+        <ButtonComponent>{FEED_PUBLISH}</ButtonComponent>
+      </section>
     </CardComponent>
   </div>
 )

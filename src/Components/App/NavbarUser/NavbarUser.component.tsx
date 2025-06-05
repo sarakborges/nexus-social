0cc5bcf943a30d2d68d9d2f5c92ecf63ec5486ba
Sara Borges <sarakborges@outlook.com>
2025-06-05 16:39:57 -0300

    layout change

--- src/Components/App/NavbarUser/NavbarUser.component.tsx
+++ src/Components/App/NavbarUser/NavbarUser.component.tsx
@@ -10,6 +10,7 @@ import { TypographyComponent } from '@/Components/System/Typography'
 import { LinkComponent } from '@/Components/System/Link'
 import { ButtonComponent } from '@/Components/System/Button'
 import { DropdownComponent } from '@/Components/System/Dropdown'
+import { CardComponent } from '@/Components/System/Card'
 
 import { NavbarUserDropdownComponent } from '@/Components/App/NavbarUserDropdown'
 
@@ -31,9 +32,9 @@ export const NavbarUserComponent = () => {
   }
 
   return (
-    <section className="navbar-user-wrapper">
-      <div className="navbar-user">
-        <ButtonComponent onClick={toggleDropdown}>
+    <section className="navbar-user">
+      <CardComponent>
+        <ButtonComponent onClick={toggleDropdown} transparent>
           <ImageComponent
             src={activeProfile?.picture || `/avatar-placeholder.png`}
             alt={activeProfile?.name}
@@ -63,7 +64,7 @@ export const NavbarUserComponent = () => {
         <DropdownComponent top left ref={navbarUserDropdownRef}>
           <NavbarUserDropdownComponent />
         </DropdownComponent>
-      </div>
+      </CardComponent>
     </section>
   )
 }
